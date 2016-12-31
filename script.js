eb57fb0eb0fa01253b2e786a91c407ef27592e69
heatherpalmer78 <heatherpa@zillow.com>
2016-12-30 18:46:23 -0800

    Early draft

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+ 
+// I know I need an onclick/submit event to change the background image 
+// based on the user input and then clicking the button
+
+// var pics = 
 
 
-if ("New York" || "NYC" || "New York City") {
 
+// document.addEventListener("submit", function changeBackground());
 
-}; else if {
+// changeBackground ()
 
+// If "this city name" is typed in, produce appropriate background image
+if (city == "New York" || city ==  "NYC" || city == "New York City") {
+  document.getElementsByClassName("nyc").style.backgroundImage="url(../images/nyc.jpg)";
 
-}; else if {
+}; else if (city == "San Francisco" || city == "SF" || city == "Bay Area"){
+  document.getElementsByClassName("sf").style.backgroundImage="url(../images/sf.jpg)";
 
+}; else if (city == "Los Angeles" || city == "LA" || city == "LAX"){
+document.getElementsByClassName("la").style.backgroundImage="url(../images/la.jpg)";
 
+}; else if (city == "Austin" || city == "ATX"){
+document.getElementsByClassName("austin").style.backgroundImage="url(../images/austin.jpg)";
+
+}; else if (city == "Sydney" || city == "SYD"){
+document.getElementsByClassName("sydney").style.backgroundImage="url(../images/sydney.jpg)";
+
+}; else (city !== "New York" || city !==  "NYC" || city !== "New York City" || 
+  city !== "San Francisco" || city !== "SF" || city !== "Bay Area" ||
+  city !== "Los Angeles" || city !== "LA" || city !== "LAX" ||
+  city !== "Austin" || city !== "ATX" || city !== "Sydney" || city !== "SYD");
+  alert("City name not programmed yet, sorry!")
 };
+)};
+
+
